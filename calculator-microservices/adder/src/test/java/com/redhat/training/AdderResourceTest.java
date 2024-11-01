e36fca251bc674a5c37ac99b8c11b1061d33f996
CORPORATE\mdiop <mdiop@teksystems.com>
2024-10-31 23:20:27 -0400

    Add adder test

--- calculator-microservices/adder/src/test/java/com/redhat/training/AdderResourceTest.java
+++ calculator-microservices/adder/src/test/java/com/redhat/training/AdderResourceTest.java
@@ -7,6 +7,11 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 @TestHTTPEndpoint(AdderResource.class)
@@ -15,5 +20,15 @@ public class AdderResourceTest {
     @InjectMock
     @RestClient 
     SolverService solverService;
+    @Test
+    public void simpleSum() {
+        Mockito.when(solverService.solve("2")).thenReturn(Float.valueOf("2"));
+        Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
 
+        given()
+                .when().get("3/2")
+                .then()
+                .statusCode(200)
+                .body(is("5.0"));
+    }
 }
